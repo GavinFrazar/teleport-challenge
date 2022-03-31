dd4979a486c0c00500b4c5a579342f70ac107766
Gavin Frazar <gavinfrazar@gmail.com>
2022-03-30 23:10:43 -0700

    Add a concurrent output test

--- joblib/src/lib.rs
+++ joblib/src/lib.rs
@@ -9,6 +9,8 @@ pub use events::JobStatus;
 
 #[cfg(test)]
 mod joblib_tests {
+    use futures::future::join_all;
+
     use super::*;
     use crate::error::Error as JobError;
 
@@ -106,5 +108,56 @@ mod joblib_tests {
             coordinator.stop_job(long_sleep_id).await,
             Err(JobError::AlreadyStopped)
         ));
+    }
+
+    #[tokio::test]
+    async fn concurrent_output() {
+        let coordinator = JobCoordinator::spawn(32);
+        let echo_str = "hello world!";
+        let no_trailing_newline = "-n";
+        let job_id = coordinator
+            .start_job(
+                "echo".into(),
+                vec![no_trailing_newline.to_string(), echo_str.to_string()],
+                "/tmp".into(),
+                vec![],
+            )
+            .await
+            .expect("job start err");
+        // get output for 3600 clients.
+        let num_subs = 3600;
+        let mut subscribers = Vec::with_capacity(num_subs);
+        for _ in 0..num_subs {
+            let mut output = coordinator
+                .stream_all(job_id)
+                .await
+                .expect("failed to grab stdout/stderr for job");
+            subscribers.push(tokio::spawn(async move {
+                let mut output_bytes = vec![];
+                while let Some(blob) = output.recv().await {
+                    output_bytes.extend(blob);
+                }
+                // Each client will wait 1 second.
+                // This is just a sanity check that the futures are being driven to completion concurrently
+                // If they didn't run concurrently, this would take an hour to run!
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                String::from_utf8_lossy(&output_bytes).into_owned()
+            }));
+        }
+        // give 10 seconds to get all the output or fail test
+        match tokio::time::timeout(std::time::Duration::from_secs(10), join_all(subscribers)).await
+        {
+            Err(_) => panic!("subscribers didnt get output concurrently"),
+            Ok(join_handles) => {
+                let received_strs: Vec<_> = join_handles
+                    .into_iter()
+                    .filter_map(|result| result.ok())
+                    .collect();
+                assert_eq!(received_strs.len(), num_subs);
+                for res in received_strs {
+                    assert_eq!(res, echo_str);
+                }
+            }
+        }
     }
 }
