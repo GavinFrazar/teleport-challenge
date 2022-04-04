97961490a5c7d7c754cd33c38d06360932503d4f
Gavin Frazar <gavinfrazar@gmail.com>
2022-04-03 18:54:24 -0700

    Move interceptor to its own module

---
+++ server/src/interceptors/cert.rs
@@ -0,0 +1,59 @@
+use protobuf::remote_jobs_server::RemoteJobsServer;
+use tonic::{
+    transport::{
+        server::{TcpConnectInfo, TlsConnectInfo},
+        Certificate, Identity, Server, ServerTlsConfig,
+    },
+    Request, Status,
+};
+use x509_parser::{
+    certificate::X509Certificate,
+    oid_registry::Oid,
+    prelude::{oid2abbrev, oid2description, oid2sn, oid_registry},
+    traits::FromDer,
+};
+
+/// A tonic interceptor service function.
+///
+/// Extracts the subject uid from the client certificate and adds it to the request extensions.
+pub fn extract_subj_uid(mut req: Request<()>) -> Result<Request<()>, Status> {
+    // extract the client certs
+    let client_certs = req
+        .peer_certs()
+        .ok_or(Status::unauthenticated("Request missing client cert"))?;
+    if client_certs.len() == 0 {
+        return Err(Status::unauthenticated("Request missing client cert"));
+    }
+
+    // get the DER encoded bytes of the cert
+    let der = client_certs[0].get_ref(); // rustls encodes the pem as der
+
+    // parse the DER bytes
+    let (rem, cert) =
+        X509Certificate::from_der(der).map_err(|_| Status::unauthenticated("Bad client cert"))?;
+    if rem.is_empty() {
+        // parse succeeded
+        let oid: &[u64] = &[0, 9, 2342, 19200300, 100, 1, 1]; // oid for the subject UID.
+        let oid = Oid::from(oid).expect("oid parse error: subject uid");
+        let uid = cert
+            .subject()
+            .iter_by_oid(&oid)
+            .take(1)
+            .next()
+            .ok_or(Status::unauthenticated("Client cert missing subject uid"))?;
+        if let x509_parser::der_parser::ber::BerObjectContent::UTF8String(user) =
+            uid.attr_value().content
+        {
+            req.extensions_mut().insert(UserExtension {
+                user_id: bytes::Bytes::copy_from_slice(user.as_bytes()),
+            });
+        } else {
+            return Err(Status::unauthenticated("Client cert uid must be UTF8"));
+        }
+    }
+    Ok(req)
+}
+
+pub struct UserExtension {
+    pub user_id: bytes::Bytes,
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 
+mod interceptors;
 mod services;
+
+pub use cert::UserExtension;
+use interceptors::cert;
 use protobuf::remote_jobs_server::RemoteJobsServer;
 use services::jobservice::RemoteJobsService;
 use tonic::{
@@ -10,16 +14,15 @@ use tonic::{
     },
     Request, Status,
 };
-use x509_parser::{
-    certificate::X509Certificate,
-    oid_registry::Oid,
-    prelude::{oid2abbrev, oid2description, oid2sn, oid_registry},
-    traits::FromDer,
-};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let addr = "[::1]:50051";
+    serve(addr).await
+}
+
+async fn serve(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let addr = addr.parse().unwrap();
 
     // load server identity
     let cert = include_bytes!("../../tls/server.cert");
@@ -35,7 +38,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .client_ca_root(ca_cert);
 
     let job_service = RemoteJobsService::default();
-    let remote_jobs_server = RemoteJobsServer::with_interceptor(job_service, extract_uid);
+    let remote_jobs_server =
+        RemoteJobsServer::with_interceptor(job_service, cert::extract_subj_uid);
     println!("Listening on {}", addr);
 
     Server::builder()
@@ -45,51 +49,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
-}
-
-fn extract_uid(mut req: Request<()>) -> Result<Request<()>, Status> {
-    println!("Intercepting request: {:?}", req);
-
-    // extract the client certs
-    let client_certs = req
-        .peer_certs()
-        .ok_or(Status::unauthenticated("Request missing client cert"))?;
-    if client_certs.len() == 0 {
-        return Err(Status::unauthenticated("Request missing client cert"));
-    }
-
-    // get the DER encoded bytes of the cert
-    let der = client_certs[0].get_ref(); // rustls encodes the pem as der
-
-    // parse the DER bytes
-    let (rem, cert) =
-        X509Certificate::from_der(der).map_err(|_| Status::unauthenticated("Bad client cert"))?;
-    if rem.is_empty() {
-        // parse succeeded
-        println!("Got cert subject info: {:?}", cert.subject().to_string());
-        let oid: &[u64] = &[0, 9, 2342, 19200300, 100, 1, 1]; // oid for the subject UID.
-        let oid = Oid::from(oid).expect("oid parse error: subject uid");
-        let uid = cert
-            .subject()
-            .iter_by_oid(&oid)
-            .take(1)
-            .next()
-            .ok_or(Status::unauthenticated("Client cert missing subject uid"))?;
-        if let x509_parser::der_parser::ber::BerObjectContent::UTF8String(user) =
-            uid.attr_value().content
-        {
-            req.extensions_mut().insert(UserExtension {
-                user_id: bytes::Bytes::copy_from_slice(user.as_bytes()),
-            });
-        } else {
-            return Err(Status::unauthenticated("Client cert uid must be UTF8"));
-        }
-    }
-    Ok(req)
-}
-
-struct UserExtension {
-    user_id: bytes::Bytes,
 }
 
 #[cfg(test)]
